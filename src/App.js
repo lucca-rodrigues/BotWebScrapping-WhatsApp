712d999f3921ed8b7e4f456b8758a43ba237b767
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2022-01-17 18:38:26 -0300

    update configs

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const config = require("./Configs/config");
 
-const main = async function (WhatsApp) {
+const main = async function () {
   const browser = await puppeteer.launch({
     headless: false,
   });
   const page = await browser.newPage();
   await page.goto(config.url, {
     waitUntil: "load",
-    // Remove the timeout
     timeout: 0,
   });
-  // await page.addScriptTag({ path: "./Scrappers/getRestaurants.js" });
-  // console.log("Listando produtos...");
 
-  // const restaurants = await page.evaluate(() => {
-  //   return restaurants;
-  // });
+  await page.addScriptTag({ path: "./src/Scrappers/getProducts.js" });
+
   console.log("Listando produtos...");
+
   const allProducts = await page.evaluate(() => {
-    const products = Array.from(document.querySelectorAll("#ad-list li")).map(
-      (item) => {
-        return {
-          title: item.querySelector("h2")?.innerText,
-          link: item.querySelector("a")?.baseURI,
-          image: item.querySelector("img")?.src,
-          price: item.querySelector(".fnmrjs-9 span")?.innerText,
-        };
-      }
-    );
     return products;
   });
+  // console.log("Listando produtos...");
+  // const allProducts = await page.evaluate(() => {
+  //   const products = Array.from(document.querySelectorAll("#ad-list li")).map(
+  //     (item) => {
+  //       return {
+  //         title: item.querySelector("h2")?.innerText,
+  //         link: item.querySelector("a")?.baseURI,
+  //         image: item.querySelector("img")?.src,
+  //         price: item.querySelector(".fnmrjs-9 span")?.innerText,
+  //       };
+  //     }
+  //   );
+  //   return products;
+  // });
 
   console.log("allProducts", allProducts);
 
